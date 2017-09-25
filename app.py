a57e8cfb736381fff59bf3ccbe86c78fb9e78537
Arthur Barros <arthbarros@gmail.com>
2017-09-25 00:11:38 -0300

    Added app.py.

--- app.py
+++ app.py
@@ -10,11 +10,15 @@ updater = Updater(telegram_token)
 
 updater.start_webhook(listen="0.0.0.0", port=port, url_path=telegram_token)
 updater.bot.setWebhook("https://alelo-bot.herokuapp.com/" + telegram_token)
-
+r = redis.StrictRedis(host=os.environ.get('REDIS_URL'), db=0)
 
 def start(bot, update):
     update.message.reply_text('Hi!')
 
+def amount_help(bot, update):
+    helps = int(r.get('help_count'))
+    helps += 1
+    r.set('helps', helps)
 
 def help(bot, update):
     update.message.reply_text('Help!')
@@ -25,5 +29,6 @@ dp = updater.dispatcher
 # on different commands - answer in Telegram
 dp.add_handler(CommandHandler("start", start))
 dp.add_handler(CommandHandler("help", help))
+dp.add_handler(CommandHandler("c_help", amount_help))
 
 updater.idle()

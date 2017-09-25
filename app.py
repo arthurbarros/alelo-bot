c6ab71b95ee88ae6622bd07cdf72aaed2281d8c1
Arthur Barros <arthbarros@gmail.com>
2017-09-25 02:50:43 -0300

    Added app.py.

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def balance(bot, update):
     token = r.get(key).decode('utf-8')
     response = requests.get('https://www.cartoesbeneficio.com.br/inst/convivencia/SaldoExtratoAlelo.jsp?ticket={}&primeiroAcesso=S&origem=Alelo'.format(token))
     balance = response.text.split('<span style="color: #008060;">')[1].split('</span>')[0]
-    update.message.reply_text('Seu saldo: R$ {}'.format(str(balance)))
+    update.message.reply_text('Seu saldo: {}'.format(str(balance)))
 
 
 dispatcher = updater.dispatcher

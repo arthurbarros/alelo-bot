30627a0bdd5a19ab050db157568b1eccbc4c4c10
Arthur Barros <arthbarros@gmail.com>
2017-09-25 02:38:16 -0300

    Added app.py.

--- app.py
+++ app.py
@@ -17,37 +17,28 @@ updater.bot.setWebhook("https://alelo-bot.herokuapp.com/" + telegram_token)
 r = redis.from_url(os.environ.get("REDIS_URL"))
 sess = {}
 
+
 def start(bot, update):
-    update.message.reply_text('Bem Vindo, por favor envie o seu token utilizando o comando de exemplo: /meu_alelo abcd1234')
+    update.message.reply_text('Bem Vindo, por favor envie o seu token utilizando o comando de exemplo: /meu_alelo 999111777222')
 
-def set_card_token(bot, update, args):
-    token = args[0]
+def meu_alelo(bot, update, args):
     user_id = update.message.from_user.id
-    key = '{}_card_token'.format(user_id)
-    r.set(key, token)
-    update.message.reply_text('O token salvo.')
-
-def get_card_token(bot, update):
-    user_id = update.message.from_user.id
-    key = '{}_card_token'.format(user_id)
-    token = r.get(key).decode('utf-8')
-    update.message.reply_text('O seu token salvo: {}'.format(str(token)))
-
-def solve(bot, update):
-    user_id = update.message.from_user.id
-    sess[user_id] = requests.Session()
+    sess[user_id] = (requests.Session(), args[0])
     res = sess[user_id].get('https://www.meualelo.com.br/inst/images/captcha.jpg', stream=True)
     res.raw.decode_content = True
+    update.message.reply_text('Informe as letras e numeros da imagem abaixo, ex: /resposta a1b2')
     update.message.reply_photo(photo=res.raw, force_reply=True)
 
 def session(bot, update, args):
     user_id = update.message.from_user.id
-    data = {'txtCartao1': '5067542566047015', 'captcha': args[0]}
-    res = sess[user_id].post('https://www.meualelo.com.br/SaldoExtratoValidacaoServlet', data=data)
+    data = {'txtCartao1': sess[user_id][1], 'captcha': args[0]}
+    res = sess[user_id][0].post('https://www.meualelo.com.br/SaldoExtratoValidacaoServlet', data=data)
     token = res.text.split('https://www.cartoesbeneficio.com.br/inst/SaldoExtratoAleloFiltro.jsp?ticket=')[1].split("'")[0]
-    print(res.text)
-    print(token)
-    # update.message.reply_text('O seu token salvo: {}'.format(str(token)))
+    user_id = update.message.from_user.id
+    key = '{}_card_token'.format(user_id)
+    r.set(key, token)
+    balance(bot, update)
+    del sess[user_id]
 
 def balance(bot, update):
     user_id = update.message.from_user.id
@@ -58,17 +49,12 @@ def balance(bot, update):
     update.message.reply_text('Seu saldo: R$ {}'.format(str(balance)))
 
 
-def help(bot, update):
-    update.message.reply_text('Help!')
-
-
 dispatcher = updater.dispatcher
 commands = [
     CommandHandler("start", start),
-    CommandHandler("meu_alelo", set_card_token, pass_args=True),
     CommandHandler("saldo", balance),
-    CommandHandler("solve", solve),
-    CommandHandler("session", session, pass_args=True)
+    CommandHandler("meu_alelo", solve, pass_args=True),
+    CommandHandler("resposta", session, pass_args=True)
 ]
 for cmd in commands:
     dispatcher.add_handler(cmd)
